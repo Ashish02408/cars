f1a89944f13162ec34698e34f5e4071dc4ef8743
unknown <ashish02408@gmail.com>
2024-03-18 18:55:32 +0530

    third

--- cars.go
+++ cars.go
@@ -1,9 +1,21 @@
 package cars
 
+import (
+	"github.com/Ashish02408/vehicle"
+)
+
 func Sound() string {
 	return "go"
 }
 
 func Sounds() string {
 	return "go go go"
+}
+
+func SoundsCrazy() string {
+	return vehicle.WhenComesUp(Sound())
+}
+
+func SoundsCrazys() string {
+	return vehicle.WhenComesUp(Sounds())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Ashish02408/cars
 
 go 1.22.1
+
+require github.com/Ashish02408/vehicle v0.0.0-20240318131724-fbc6f28c619b // indirect
